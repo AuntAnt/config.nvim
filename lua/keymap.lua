d6fe75a7f12538ee9c2cac5cf60da6fde226c557
AntKzmn <furthertwoant@gmail.com>
2026-04-24 11:05:15 +0400

    changed comment keymap

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -2,6 +2,5 @@
 vim.keymap.set("n", "<leader>e", "<Cmd>Explore<CR>")
 
 -- Comment line on CTRL+C
-vim.keymap.set("n", "<C-c>", "gcc", { remap = true })
-vim.keymap.set("v", "<C-c>", "gcc", { remap = true })
+vim.keymap.set({"n", "v"}, "<C-c>", "gcc", { remap = true })
 
