bd34c93f40ede690e9474b03e19535531cb59a67
AntKzmn <furthertwoant@gmail.com>
2026-05-03 21:46:09 +0400

    added autopairs plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -87,3 +87,11 @@ vim.pack.add({
 	{ src = "https://github.com/folke/which-key.nvim" },
 })
 
+-- autopairs
+vim.pack.add({
+	{ src = "https://github.com/windwp/nvim-autopairs" },
+})
+
+require("nvim-autopairs").setup {
+	event = "InsertEnter"
+}
