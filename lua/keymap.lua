1b2d65a8c3cd5f80ac77d167cae7fdfef702697d
AntKzmn <furthertwoant@gmail.com>
2026-06-15 10:36:04 +0400

    start/end line keymap

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -4,6 +4,10 @@ vim.keymap.set("n", "<leader>e", "<Cmd>Explore<CR>")
 -- Comment line on CTRL+C
 vim.keymap.set({"n", "v"}, "<C-c>", "gcc", { remap = true })
 
+
+vim.keymap.set({"n", "v"}, "gh", "g^", { desc = "End of line" })
+vim.keymap.set({"n", "v"}, "gl", "g_", { desc = "Start of line" })
+
 -- fff find files
 vim.keymap.set("n", "ff", function()
 	require("fff").find_files()
